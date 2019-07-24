0432a0a63b06c16bc1a97437c3c3e19bcfd572c0
jonathantang <jonathantang0126@gmail.com>
2019-07-24 22:43:23 +0800

    modify render function

--- main.js
+++ main.js
@@ -1,39 +1,74 @@
 const BASE_URL = 'https://spreadsheets.google.com/feeds/cells/1J19Q-C_KGpNqEqJVYkR0-CH1aOgBZh-LPkQOvSSOYNk/1/public/values?alt=json'
 
 const dataPanel = document.getElementById('data-panel')
+const colNumber = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I']
+console.log(colNumber)
 
 const data = []
+const itemArray = []
+const boxLaction = []
+const itemInBox = []
+
+defaultBoxItem()
 
 axios.get(BASE_URL)
   .then((response) => {
     console.log(response)
     data.push(...response.data.feed.entry)
     console.log(data)
-    console.log(data[0].gs$cell.$t)
-    displayBoxItem(data)
+    data.forEach(item => {
+      if (item.gs$cell.col === '1') {
+        itemArray.push(item.gs$cell.$t)
+      } else if (item.gs$cell.col === '2') {
+        boxLaction.push(item.gs$cell.$t)
+      }
+
+    })
+    console.log(itemArray)
+    console.log(boxLaction)
+    arrangeItem(itemArray, boxLaction)
+    putItemInBox(itemInBox)
+    //displayBoxItem(data)
   })
   .catch((err) => {
     console.log(err)
   })
 
-function displayBoxItem(data) {
+function defaultBoxItem() {
   let htmlContent = `
-    <button type="button" class="btn btn-light">A</button>
-    <button type="button" class="btn btn-light">B</button>
-    <button type="button" class="btn btn-light">C</button>
-    <button type="button" class="btn btn-light">D</button>
-    <button type="button" class="btn btn-light">E</button>
-    <button type="button" class="btn btn-light">F</button>
-    <button type="button" class="btn btn-light">G</button>
-    <button type="button" class="btn btn-light">H</button>
-    <button type="button" class="btn btn-light">I</button>
+    <button type="button" class="btn btn-light">1</button>
+    <button type="button" class="btn btn-light">2</button>
+    <button type="button" class="btn btn-light">3</button>
+    <button type="button" class="btn btn-light">4</button>
+    <button type="button" class="btn btn-light">5</button>
+    <button type="button" class="btn btn-light">6</button>
+    <button type="button" class="btn btn-light">7</button>
+    <button type="button" class="btn btn-light">8</button>
+    <button type="button" class="btn btn-light">9</button>
   `
-
-  data.forEach(item => {
-    htmlContent += `
-      <button type="button" class="btn btn-outline-primary">${item.gs$cell.$t}</button>
+  for (let x = 0; x < colNumber.length; x++) { //col
+    for (let y = 1; y < 10; y++) {
+      htmlContent += `
+      <button type="button" class="btn btn-outline-primary" id="${colNumber[x]}${y}">${colNumber[x]}${y}</button>
     `
-  })
+    }
+  }
   dataPanel.innerHTML = htmlContent
+}
 
+function arrangeItem(array1, array2) {
+  for (let i = 0; i < array1.length; i++) {
+    itemInBox.push({ name: array1[i], location: array2[i] })
+  }
+  delete itemInBox[0]
+  console.log(itemInBox)
+}
+
+function putItemInBox(array) {
+  array.forEach(item => {
+    const boxTarget = document.getElementById(item.location)
+    console.log(boxTarget)
+    boxTarget.classList.add('active')
+    boxTarget.innerText = item.name
+  });
 }
